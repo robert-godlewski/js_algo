34b0da17c6db519b35f06c1bd2210fa1f4daf888
Robert Godlewski <robtechg5@gmail.com>
2022-06-04 16:28:18 -0700

    Unsuccessful attempt at trying to merge 2 arrays.

--- array_solutions/arrsol202206.js
+++ array_solutions/arrsol202206.js
@@ -70,3 +70,36 @@ var duplicateZeros = function(arr) {
         }
     }
 };
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+// This fails - Need to fix
+var merge = function(nums1, m, nums2, n) {
+    if (n > 0) {
+        var i = 0;
+        var i_num_2 = 0;
+        while (nums2[nums2.length-1] != 0) {
+            console.log(nums1);
+            console.log(i);
+            if (nums1[i] == 0) {
+                nums1[i] = nums2[i_num_2];
+                nums2[i_num_2] = 0;
+                i_num_2++;
+            } else if (nums1[i] > nums2[i_num_2]) {
+                var temp = nums1[i];
+                nums1[i] = nums2[i_num_2];
+                if (temp < nums2[i_num_2+1]) {
+                    nums2[i_num_2] = temp;
+                } else {
+                    nums2[i_num_2] = 0;
+                    i_num_2++;
+                    nums2.push(temp);
+                }
+            } else if (i == (m+n)) {
+                i = 0;
+            }
+            i++;
+        }
+    }
+}
