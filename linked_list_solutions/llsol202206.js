14ab46471e05eeeedf7cd5287f9aada1cfdec54d
Robert Godlewski <robtechg5@gmail.com>
2022-06-12 16:10:56 -0700

    Another attempt at solving a different Linked List problem.

--- linked_list_solutions/llsol202206.js
+++ linked_list_solutions/llsol202206.js
@@ -34,6 +34,31 @@ class LinkedList {
             }
         }
     }
+
+    // Given the head of a linked list, remove the nth node from the end of the list and return its head.
+    // head parameter is a node list
+    // n is a number of nodes that is away from the tail of the list
+    // This solution doesn't work
+    removeNthFromEnd(head, n) {
+        var current = head;
+        var prev = null;
+        var len = 1;
+        while (current) {
+            //console.log(`current node value = ${current.val}`);
+            prev = current;
+            current = current.next;
+            len++;
+        }
+        var i = 0;
+        current = head;
+        while (i < len-n) {
+            prev = current;
+            current = current.next;
+            i++;
+        }
+        prev.next = current.next;
+        return head;
+    }
 }
 
 
@@ -67,4 +92,21 @@ console.log('Original Linked List:');
 printLL(ll);
 console.log('Removing 5 from the list');
 ll.deleteNode(node2);
+printLL(ll);
+console.log('Removing the Nth node from the Linked List:');
+ll.head = null;
+var node1 = new Node(1);
+var node2 = new Node(2);
+node1.next = node2;
+var node3 = new Node(3);
+node2.next = node3;
+var node4 = new Node(4);
+node3.next = node4;
+var node5 = new Node(5);
+node4.next = node5;
+ll.head = node1;
+console.log('Original Linked List:');
+printLL(ll);
+ll.removeNthFromEnd(this, 2);
+console.log('Removing the 2nd last node from the list:');
 printLL(ll);
