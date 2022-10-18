3781a6c36d544b5abd7a54735dad57b0321af507
Robert Godlewski <robtechg5@gmail.com>
2022-10-18 13:08:47 -0700

    Finally found a solution for isIsomorphic hash map in js.

--- hash_solutions/hashmap_solutions202210.js
+++ hash_solutions/hashmap_solutions202210.js
@@ -42,6 +42,53 @@ var twoSum = function(nums, target) {
 };
 
 
+// Solved in over 30 min, Ended up looking up solution
+// Best case: O(1) time and space solution
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    // Base Case, They are not isomorphic if the 2 strings are not the same length
+    if (s.length != t.length) {
+        return false;
+    };
+    // Create 2 hash maps for s to t and t to s
+    var s_t_map = {};
+    var t_s_map = {};
+    // Looping through each index of both strings
+    for (var i = 0; i < s.length; i++) {
+        //console.log(i);
+        //console.log(s[i]);
+        //console.log(t[i]);
+        // Comparing the hash maps at the letter of both strings
+        if (!s_t_map[s[i]] && !t_s_map[t[i]]) {
+            // Adds in if each letter is not in the hashes.
+            s_t_map[s[i]] = {
+                "corres": t[i],
+                "indexes": [i]
+            };
+            t_s_map[t[i]] = {
+                "corres": s[i],
+                "indexes": [i]
+            };
+        } else if (s_t_map[s[i]] && t_s_map[t[i]]) {
+            // Updates the list of indexes that each one maps to only if the corres(ponding) is equal to eachother
+            if (s_t_map[s[i]]["corres"] === t[i] && t_s_map[t[i]]["corres"] === s[i]) {
+                s_t_map[s[i]]["indexes"].push(i);
+                t_s_map[t[i]]["indexes"].push(i);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        };
+    };
+    return true;
+};
+
+
 // Testing Area
 console.log('#############');
 console.log('Hash Map Algorithms');
@@ -56,3 +103,14 @@ var twoSumTest = function(nums, target) {
 twoSumTest([2,7,11,15], 9);
 twoSumTest([3,2,4], 6);
 twoSumTest([3,3], 6);
+
+// Testing isIsomorphic
+console.log('--------');
+var isomorphicTest = function(s, t) {
+    console.log(`Is ${s} and ${t} isomorphic?`);
+    var isobool = isIsomorphic(s, t);
+    console.log(isobool);
+};
+isomorphicTest("egg", "add");
+isomorphicTest("foo", "bar");
+isomorphicTest("paper", "title");
